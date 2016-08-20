51398f0786738cff37257cc61eb24e39664fb526
Mater404 <Mater404@outlook.com>
2016-08-20 14:13:24 +0800

    添加生成二维码

--- Zxingapp/src/main/java/com/app/zxingtest/MainActivity.java
+++ Zxingapp/src/main/java/com/app/zxingtest/MainActivity.java
@@ -2,18 +2,28 @@ package com.app.zxingtest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.uuzuche.lib_zxing.activity.CaptureActivity;
+import com.uuzuche.lib_zxing.activity.CodeUtils;
 
 public class MainActivity extends Activity implements View.OnClickListener{
     private Button button1 = null;
+    private Button button2 = null;
     private TextView mTvResult;
+    private EditText mInput;
+    private ImageView mResult;
+    private CheckBox mLogo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +34,13 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     private void initView() {
         button1 = (Button) findViewById(R.id.Button1);
+        button2 = (Button) findViewById(R.id.Button2);
         mTvResult = (TextView) findViewById(R.id.tv_result);
+        mInput = (EditText) findViewById(R.id.et_text);
+        mResult = (ImageView) findViewById(R.id.LogImage);
+        mLogo = (CheckBox) findViewById(R.id.cb_logo);
         button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
     }
 
     @Override
@@ -35,6 +50,16 @@ public class MainActivity extends Activity implements View.OnClickListener{
                 Log.d("abc", "Button1");
                 startActivityForResult(new Intent(MainActivity.this, CaptureActivity.class),0);
                 break;
+            case R.id.Button2:
+                String input = mInput.getText().toString();
+                if(input.equals("")){
+                    Toast.makeText(MainActivity.this,"输入不能为空",Toast.LENGTH_LONG).show();
+                }else {
+                    Bitmap bitmap = CodeUtils.createImage(input,400,400,
+                            mLogo.isChecked()?
+                            BitmapFactory.decodeResource(getResources(),R.drawable.logo):null);
+                    mResult.setImageBitmap(bitmap);
+                }
         }
     }
 
@@ -43,7 +68,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK){
             Bundle bundle = data.getExtras();
-            String resule = bundle.getString("result");
+            String resule = bundle.getString(CodeUtils.RESULT_STRING);
             mTvResult.setText(resule);
             Toast.makeText(MainActivity.this,resule+"----",Toast.LENGTH_LONG).show();
             Log.d("abc",resule+"dsfdsf");
